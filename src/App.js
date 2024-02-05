1686304a972bc8268753be3a27c07ed50051353f
rareseduard <141629369+rareseduard@users.noreply.github.com>
2024-02-05 20:55:00 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blog/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
